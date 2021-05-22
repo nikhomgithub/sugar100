efd24f302e18c729ce1c0290ea86ff810284a91c
nikhomgithub <mr_nikom@yahoo.com>
2021-05-22 15:00:32 +0700

    first commit

---
+++ client/build/precache-manifest.5dd5d662444f404ce5b74270b518460c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a071434c85935c673fc3281b819a234",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2641e347e18f5f0c3d98",
+    "url": "/static/css/2.30e53678.chunk.css"
+  },
+  {
+    "revision": "0f943987fb395f9fe28e",
+    "url": "/static/css/main.79be1510.chunk.css"
+  },
+  {
+    "revision": "2641e347e18f5f0c3d98",
+    "url": "/static/js/2.145b3172.chunk.js"
+  },
+  {
+    "revision": "3398230d029e53296f78fca228d6d363",
+    "url": "/static/js/2.145b3172.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f943987fb395f9fe28e",
+    "url": "/static/js/main.baa8faa4.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  },
+  {
+    "revision": "b41b655e569ec068860676f3c826314c",
+    "url": "/static/media/forest.b41b655e.jpg"
+  },
+  {
+    "revision": "96feb6467e1f7bcefb042ba5df4d105f",
+    "url": "/static/media/icecream.96feb646.jpg"
+  },
+  {
+    "revision": "7891870d284af26e24a343fc74dff4ca",
+    "url": "/static/media/line.7891870d.jpg"
+  },
+  {
+    "revision": "5cafec298a92a2e4ff2f01f06beb32c0",
+    "url": "/static/media/mysquarephoto.5cafec29.png"
+  },
+  {
+    "revision": "e379c494d0e78f2c5eacbd148d273a48",
+    "url": "/static/media/waterfall.e379c494.jpg"
+  },
+  {
+    "revision": "0b57d847ebc3f9ea9036f94a086eacd6",
+    "url": "/static/media/whatapp.0b57d847.jpg"
+  }
+]);
